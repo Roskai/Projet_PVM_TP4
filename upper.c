2dab955758765fa61f6b76e5bbbe2019e1ed21f5
Angel Gezat <angel.gezat@etudiant.univ-brest.fr>
2024-02-02 10:43:01 +0100

    modified:   upper.c

--- upper.c
+++ upper.c
@@ -21,36 +21,37 @@
  */
 void upper_hull(point *pts)
 {
-	point *upper, *pts2;
+    point *upper, *pts2;
 
-	upper = point_UH(pts); /* retourne 0 si plus de 4 points */
-	if (!upper) {
-		pts2 = point_part(pts);
-		upper_hull(pts);
-		upper_hull(pts2);
-		point_merge_UH(pts, pts2);
-	}
+    upper = point_UH(pts); /* retourne 0 si plus de 4 points */
+    if (!upper)
+    {
+        pts2 = point_part(pts);
+        upper_hull(pts);
+        upper_hull(pts2);
+        point_merge_UH(pts, pts2);
+    }
 }
 
 /*
  * upper <nb points>
  * exemple :
- * % upper 200 
+ * % upper 200
  * % evince upper_hull.pdf
  */
 int main(int argc, char **argv)
 {
-	point *pts;
+    point *pts;
 
-	if (argc != 2) {
-		fprintf(stderr, "usage: %s <nb points>\n", *argv);
-		exit(-1);
-	}
-	pts = point_random(atoi(argv[1]));
-	point_print_gnuplot(pts, 0); /* affiche l'ensemble des points */
-	upper_hull(pts);
-	point_print_gnuplot(pts, 1); /* affiche l'ensemble des points restant, i.e
-					l'enveloppe, en reliant les points */
-	point_free(pts); 
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: %s <nb points>\n", *argv);
+        exit(-1);
+    }
+    pts = point_random(atoi(argv[1]));
+    point_print_gnuplot(pts, 0); /* affiche l'ensemble des points */
+    upper_hull(pts);
+    point_print_gnuplot(pts, 1); /* affiche l'ensemble des points restant, i.e
+                    l'enveloppe, en reliant les points */
+    point_free(pts);
 }
-
